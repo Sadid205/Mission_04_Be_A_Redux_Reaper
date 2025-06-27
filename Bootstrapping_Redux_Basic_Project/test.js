4dbc3d96991d58f602a33d827808ca615377dcd7
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-06-27 15:01:36 +0600

    22-12

---
+++ Bootstrapping_Redux_Basic_Project/test.js
@@ -0,0 +1,7 @@
+//* Pure function
+
+const add = (a, b) => a + b;
+
+console.log(add(2, 5));
+console.log(add(2, 5));
+console.log(add(2, 5));
--- Intro_to_redux/explore_redux/src/pages/Tasks.tsx
+++ Intro_to_redux/explore_redux/src/pages/Tasks.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TaskCard";
 import { selectFilter, selectTasks } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -9,7 +10,10 @@ export default function Tasks() {
   //   console.log(filter);
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
-      <div>Tasks</div>
+      <div className="flex justify-between items-center">
+        <h1>Tasks</h1>
+        <AddTaskModal />
+      </div>
       <div className="space-y-5 mt-5">
         {tasks.map((task) => (
           <TaskCard task={task} />
